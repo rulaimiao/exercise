1a9b81a1988e1a6eb5e2b6401219e621931ea87f
miaorulai <rulaimiao@163.com>
2018-01-05 00:28:13 +0800

    project_two

---
+++ project1/two.py
@@ -0,0 +1,25 @@
+
+'''
+ slice 
+ 用来创建一个切片对象。。。
+ 作用：
+ 对代码中进行字符串，列表等提取的内容进行切片索引进行命名
+ 便于后期维护
+'''
+
+items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+one = slice(2,8, 2)
+
+s = items[one]
+print(s)
+print(one.start)
+print(one.stop)
+print(one.step)
+
+two = slice(0, 10,2)
+ss = 'nishibushiwozuishenaideren'
+sss = two.indices(len(ss))
+print(type(sss))
+print(sss)
+for i in range(*sss):
+    print(ss[i])
